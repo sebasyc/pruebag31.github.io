21bddcde9862df5c6c6e2bf8c98ed873f01a39e9
Sebastian Campos <pseba@outlook.com>
2020-05-10 01:43:20 -0400

    Avance de portafolio en sobre mi y experiencia

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,21 @@ $(document).ready( function () {
 
     $(window).scroll(function() {
 		if($(this).scrollTop() > 100) {
-			$('header').addClass('gradient')
+			$('header').addClass('head-change')
 		} else if ($(this).scrollTop() < 10) {
-			$('header').removeClass('gradient')
+			$('header').removeClass('head-change')
 		}
     })
+
     $('.navbar-toggler').click( function () {
         $('header').toggleClass('menu-active')
     })
     
 })
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
